7570b5761017abdc7ba18b9db7f2103097b53841
Andrew Nugged <nugged@gmail.com>
2022-05-24 22:58:45 +0300

    Make build script to be MacOS compatible (requires homebrew 'gsed')

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ PM_FILE='Koha/Plugin/Fi/NatLibFi/Biblios.pm'
 DATE=`date +"%Y-%m-%d"`
 VERSION=`git log -1 --pretty=oneline --decorate | egrep -o "tag: v[0-9.]+" | sed -e "s/tag: v//"`
 RELEASE_FILE="koha-plugin-rest-biblios-${VERSION}.kpz"
-if [ ! -z "$VERSION" ]
-then
+if [ ! -z "$VERSION" ]; then
   echo "Building release package ${RELEASE_FILE}"
   if [ -e ${RELEASE_FILE} ]
   then
@@ -13,8 +12,13 @@ then
   fi
   mkdir dist
   cp -r Koha dist/.
-  sed -i -e "s/{VERSION}/${VERSION}/g" "dist/${PM_FILE}"
-  sed -i -e "s/1900-01-01/${DATE}/g" "dist/${PM_FILE}"
+
+  sed=sed
+  # make MacOS GNU sed build possible if available:
+  [ "$(uname -s)" == "Darwin" ] && which gsed >/dev/null && sed=gsed
+
+  $sed -i -e "s/{VERSION}/${VERSION}/g" "dist/${PM_FILE}"
+  $sed -i -e "s/1900-01-01/${DATE}/g" "dist/${PM_FILE}"
   cd dist
   zip -r ../${RELEASE_FILE} ./Koha
   cd ..
